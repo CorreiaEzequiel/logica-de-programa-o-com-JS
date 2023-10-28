6d523e3026986fb6bb11b99d404ced0230642fc1
Ezequiel <contato.ezequielcorreia@gmail.com>
2023-10-28 11:02:14 -0300

    alterando codigo js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 alert("Boas vindas ao jogo de adivinhação");
+console.log("O número secreto é 12");
 let numeroSecreto = 12;
+let chute;
+let tentativas = 1;
 
-chute = prompt("Escolha um numero entre 0 e 50");
+while (chute != numeroSecreto) {
+  chute = prompt("Escolha um número entre 0 e 50");
 
-if (chute == numeroSecreto) {
-  alert("Parabens você acertou");
-} else {
-  alert("Ops, você errou !");
+  if (chute == numeroSecreto) {
+    alert(
+      `Parabéns, você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`
+    );
+  } else if (chute > numeroSecreto) {
+    alert("Você errou, o número secreto é menor");
+  } else {
+    alert("Você errou, o número secreto é maior");
+  }
+  tentativas++;
 }
