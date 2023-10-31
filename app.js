1eda99aa3f52bef359c8cc608c7c3451e051558c
Ezequiel Correia <contato.ezequielcorreia@gmail.com>
2023-10-31 01:47:10 -0300

    Adicionando contador no codigo JS

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 alert("Bem vindo ao jogo de adivinhação");
 let numeroSecreto = 14;
 let chute;
+let tentativas = 1;
 
 while (chute != numeroSecreto) {
   chute = prompt("Digite um numero de 1 a 15");
   if (chute == numeroSecreto) {
-    alert(`Parabés, você descobriu o numero secreto ${chute}`);
+    alert(
+      `Parabés, você descobriu o numero secreto ${chute} com ${tentativas} tentativas.`
+    );
   } else if (chute < numeroSecreto) {
     alert(`Você errou, o numero secreto é maior que ${chute}`);
   } else {
     alert(`Você errou, o numero secreto é menor que ${chute}`);
   }
+  tentativas++;
 }
